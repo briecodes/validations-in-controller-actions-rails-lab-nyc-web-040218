11b269da75a97f60ad2a49250a9479c2d08b997f
briecodes <hello@brie.work>
2018-04-30 13:17:49 -0400

    in-controller actions defined.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,15 +2,22 @@ class PostsController < ApplicationController
   before_action :set_post!, only: [:show, :edit, :update]
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+    @post_test = Post.new(post_params)
+    @post = Post.find(params[:id])
+    if @post_test.valid?
+      @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
